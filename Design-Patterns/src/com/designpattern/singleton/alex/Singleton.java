c79d5669bfa429c0e0a29409df7ab08a968a5c28
Alexandre Simonin <alsimonin@assystemtechnologies.com>
2019-04-08 11:30:43 +0200

    commit alex

--- Design-Patterns/src/com/designpattern/singleton/alex/Singleton.java
+++ Design-Patterns/src/com/designpattern/singleton/alex/Singleton.java
@@ -1,10 +1,21 @@
 package com.designpattern.singleton.alex;
 
-public class Singleton {
-
-	
-	private void singleton() {
-		
-		
-	}
+public final class Singleton
+{       
+    /** Constructeur privé */  
+    private Singleton()
+    {}
+     
+    /** Holder */
+    private static class SingletonHolder
+    {       
+        /** Instance unique non préinitialisée */
+        private final static Singleton instance = new Singleton();
+    }
+ 
+    /** Point d'accès pour l'instance unique du singleton */
+    public static Singleton getInstance()
+    {
+        return SingletonHolder.instance;
+    }
 }
